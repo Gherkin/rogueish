2efa69da5ff640f1ccba1a491bc48fee5256753e
niyohn <niyohn@stekligan.se>
2024-02-18 15:21:52 +0100

    the screen resolution now scales (not very well), and pause works again

--- Level.gd
+++ Level.gd
@@ -51,7 +51,7 @@ func create():
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	process_mode = Node.PROCESS_MODE_PAUSABLE
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
